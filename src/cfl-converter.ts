c9dffb4ef3d7dd301cb3fd03ad59c9d07c9db985
David Katz <dhkatz.16@gmail.com>
2019-04-19 22:05:55 +0000

    Add support for supplying product IDs

--- src/cfl-converter.ts
+++ src/cfl-converter.ts
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
 import program from 'commander';
+import download from 'download';
 import fs from 'fs';
 import JSZip from 'jszip';
 import lzma from 'lzma-purejs';
@@ -22,17 +24,37 @@ async function main() {
   program
   .version('0.0.1')
   .option('-I, --input <files>', '.CFL file(s) to convert.', (value: string): string[] => value.split(','))
+  .option('-P, --products <ids>', 'List of Product IDs to retrieve and convert.', (value: string) => value.split(','))
   .parse(process.argv);
 
-  const inputs: string[] = program.input;
+  const products: Buffer[] = (await Promise.all((program.products || []).map(async (product: string): Promise<Buffer | null> =>  { 
+    for (let i = 100; i > 0; i--) {
+      try {
+        const file = await download(`http://userimages-akm.imvu.com/productdata/${product}/${i}`);
 
+        return Buffer.from(file);
+      } catch {
+        continue;
+      }
+    }
+
+    return null;
+  })) as Array<Buffer | null>).filter((value: Buffer | null) => value != null);
+
+  const inputs: Array<string | Buffer> = [...program.input || [], ...products];
+
+  console.log(chalk.yellowBright(`Beginning processing on ${inputs.length} file${inputs.length > 1 ? 's' : ''}. . .\n`));
+
+  let count = 0;
   for (const input of inputs) {
-    if (!input.toLocaleLowerCase().endsWith('.cfl')) {
-      console.error('This program only supports files ending with the .CFL extension!');
+    if (typeof input === 'string' && !input.toLocaleLowerCase().endsWith('.cfl')) {
+      console.log(chalk.redBright('This program only supports files ending with the .CFL extension!'));
       return;
     }
 
-    const reader = new BinaryReader(fs.readFileSync(input));
+    console.log(chalk.blueBright(`Processing '${typeof input === 'string' ? input : 'download'}'. . .`));
+
+    const reader = new BinaryReader(typeof input === 'string' ? fs.readFileSync(input) : input);
 
     // CFL Header
     // This information is exclusive to the CFL format
@@ -43,9 +65,13 @@ async function main() {
 
     const format = reader.readInt();
 
+    console.log(chalk.greenBright(`\tDecoded CFL header.`));
+
     // Decompress the LZMA section of the CFL file
     const content = Uint8Array.from([...Array(reader.readInt())].map(() => reader.readByte()));
     const data = new BinaryReader(format === 4 ? decompress(content) : content);
+
+    console.log(chalk.greenBright(`\tDecompressed CFL content.`));
 
     // Parse the CFL entries
     const entries: CFLEntry[] = [];
@@ -81,6 +107,10 @@ async function main() {
       position += length;
     }
 
+    console.log(chalk.greenBright(`\tParsed ${entries.length} CFL ${entries.length !== 1 ? 'entries': 'entry'}.`));
+
+    count++;
+
     const zip = new JSZip();
 
     entries.forEach((entry: CFLEntry) => {
@@ -88,9 +118,11 @@ async function main() {
     });
 
     zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-      .pipe(fs.createWriteStream(`${input.slice(0, -4)}.chkn`))
-      .on('close', () => console.log(`Wrote to ${input.slice(0, -4)}.chkn`));
+      .pipe(fs.createWriteStream(`${typeof input === 'string' ? input.slice(0, -4) : count}.chkn`))
+      .on('close', () => console.log(chalk.greenBright(`\tWrote to ${typeof input === 'string' ? input.slice(0, -4) : count}.chkn.\n`)));
   }
+
+  console.log(`Finished processing ${inputs.length} file${inputs.length > 1 ? 's' : ''}!`)
 }
 
 function decompress(data: Uint8Array | Buffer): Uint8Array {
