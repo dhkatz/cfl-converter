4bccc6ab85ca05884cf76839b9a0761761173cf0
dhkatz <dhkatz.16@gmail.com>
2021-09-30 11:34:27 -0700

    Update program to support downloading products.

--- src/cfl-converter.ts
+++ src/cfl-converter.ts
@@ -1,109 +1,182 @@
 #!/usr/bin/env node
-import fs from 'fs';
+import fs from 'fs'
 
-import { convert } from '.';
+import { convert } from '.'
 
-import program from "commander";
-import download from "download";
+import { buffer as toBuffer } from 'get-stream'
+import { Command } from 'commander'
+import download from 'download'
+import JSZip from 'jszip'
+
+interface Options {
+  input: string[]
+  quiet: boolean
+  products: string[]
+  ignoreMissing: boolean
+  ignoreExtension: boolean
+  dryRun: boolean
+}
 
 async function main() {
+  const program = new Command()
+
   program
     .version('1.0.0')
-    .option('-I, --input <files>', '.CFL file(s) to convert.', (value: string): string[] => value.split(','), [])
-    .option('-P, --products <ids>', 'List of Product IDs to retrieve and convert.', (value: string) => value.split(','), [])
+    .option(
+      '-I, --input <files>',
+      '.CFL file(s) to convert.',
+      (value: string): string[] => value.split(','),
+      []
+    )
+    .option(
+      '-P, --products <ids>',
+      'List of Product IDs to retrieve and convert.',
+      (value: string) => value.split(','),
+      []
+    )
     .option('-Q, --quiet', 'Stop the program from logging progress.', Boolean, false)
-    .option('-D, --dry-run', 'Skip outputting to a file and just attempt to convert.', Boolean, false)
+    .option(
+      '-D, --dry-run',
+      'Skip outputting to a file and just attempt to convert.',
+      Boolean,
+      false
+    )
     .option('--ignore-missing', 'Ignore IDs that point to non-existent products.', Boolean, false)
-    .option('--ignore-extension', 'Attempt to process all files regardless of extension.', Boolean, false);
+    .option(
+      '--ignore-extension',
+      'Attempt to process all files regardless of extension.',
+      Boolean,
+      false
+    )
 
   program.on('--help', () => {
-    console.log('');
-    console.log('Examples:');
-    console.log('  $ cfl-converter --input product.cfl,chair.cfl');
-    console.log('  $ cfl-converter --products 1243456,654321');
-  });
+    console.log('')
+    console.log('Examples:')
+    console.log('  $ cfl-converter --input product.cfl,chair.cfl')
+    console.log('  $ cfl-converter --products 1243456,654321')
+  })
 
-  program.parse(process.argv);
+  program.parse(process.argv)
+
+  if (process.argv.length < 3) {
+    program.help()
+  }
+
+  const options = program.opts<Options>()
 
   async function downloadProduct(id: number | string): Promise<Buffer | null> {
-    let i = 100;
+    const pid = typeof id === 'number' ? id : id.trim()
+    let i = 100
     for (; i > 0; i--) {
       try {
-        const file = await download(`http://userimages-akm.imvu.com/productdata/${typeof id === 'number' ? id : id.trim()}/${i}`);
+        const file = await download(`https://userimages-akm.imvu.com/productdata/${pid}/${i}`)
 
-        return Buffer.from(file);
-      } catch {
-
-      }
+        if (file.length > 0) break
+      } catch {}
     }
 
-    return null;
+    if (i === 0) return null
+
+    const contents = await download(
+      `https://userimages-akm.imvu.com/productdata/${pid}/${i}/_contents.json`
+    )
+    const manifest = JSON.parse(contents.toString())
+
+    const zip = new JSZip()
+
+    for (const file of manifest) {
+      const url = file.url ?? file.name
+      console.log(`Downloading https://userimages-akm.imvu.com/productdata/${pid}/${i}/${url}`)
+      const data = await download(`https://userimages-akm.imvu.com/productdata/${pid}/${i}/${url}`)
+
+      zip.file(file.name, data)
+    }
+
+    return toBuffer(zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }))
   }
 
-  const options = {
-    input: program.input as string[],
-    log: !program.quiet,
-    products: program.products as string[],
-  };
-
-  if (options.products.length > 0 && options.log) {
-    console.log(`Downloading ${options.products.length} CFL files...\n`);
+  if (options.products.length > 0 && !options.quiet) {
+    console.log(`Downloading ${options.products.length} CFL files...\n`)
   }
 
-  const products = await Promise.all(options.products.map(downloadProduct));
+  const products = await Promise.all(options.products.map(downloadProduct))
 
-  if (!program.ignoreMissing) {
+  if (!options.ignoreMissing) {
     products.forEach((value) => {
       if (value == null) {
-        console.error('One or more CFLs failed to download! (Use the --ignore-missing flag to override)');
+        console.error(
+          'One or more CFLs failed to download! (Use the --ignore-missing flag to override)'
+        )
       }
-    });
+    })
   }
 
-  if (!program.ignoreExtension) {
+  products.forEach((product, i) => {
+    if (!options.dryRun) {
+      fs.writeFileSync(`${options.products[i]}.chkn`, product)
+
+      if (!options.quiet) {
+        console.log(`Wrote to '${options.products[i]}.chkn'.\n`)
+      }
+    } else if (!options.quiet) {
+      console.log(`Successfully downloaded '${options.products[i]}'.\n`)
+    }
+  })
+
+  if (!options.quiet && options.products.length > 0) {
+    console.log(`Successfully downloaded ${options.products.length} file(s)!`)
+  }
+
+  if (!options.ignoreExtension) {
     options.input.forEach((value) => {
       if (!value.toLocaleLowerCase().endsWith('.cfl')) {
-        console.error('This program only supports files with the .CFL extension! (Use the --ignore-extension flag to override)');
-        process.exit(1);
+        console.error(
+          'This program only supports files with the .CFL extension! (Use the --ignore-extension flag to override)'
+        )
+        process.exit(1)
       }
-    });
+    })
   }
 
-  const inputs = [...options.input.map((input) => fs.readFileSync(input.trim())), ...products];
-  const names = [...options.input.map((input) => input.toLocaleLowerCase().endsWith('.cfl') ? input.slice(0, -4) : input), ...options.products];
+  const inputs = options.input.map((input) => fs.readFileSync(input.trim()))
+  const names = options.input.map((input) =>
+    input.toLocaleLowerCase().endsWith('.cfl') ? input.slice(0, -4) : input
+  )
 
-  if (options.log) {
-    console.log(`Beginning processing on ${inputs.length} file(s). . .\n`);
+  if (!options.quiet && inputs.length > 0) {
+    console.log(`Beginning processing on ${inputs.length} file(s). . .\n`)
   }
 
-  await Promise.all(inputs.map(async (input, index) => {
-    if (input == null) {
-      return;
-    }
+  await Promise.all(
+    inputs.map(async (input, index) => {
+      if (input == null) {
+        return
+      }
 
-    if (options.log) {
-      console.log(`Processing file ${names[index]}. . .`);
-    }
+      if (!options.quiet) {
+        console.log(`Processing file ${names[index]}. . .`)
+      }
 
-    try {
-      const file = await convert(input, { log: options.log });
+      try {
+        const file = await convert(input, { log: !options.quiet })
 
-      if (!program.dryRun) {
-        fs.writeFileSync(`${names[index]}.chkn`, file);
-        if (options.log) {
-          console.log(`Wrote to ${names[index]}.chkn.\n`);
+        if (!options.dryRun) {
+          fs.writeFileSync(`${names[index]}.chkn`, file)
+          if (!options.quiet) {
+            console.log(`Wrote to '${names[index]}.chkn'.\n`)
+          }
+        } else if (!options.quiet) {
+          console.log(`Successfully converted '${names[index]}'.\n`)
         }
-      } else if (options.log) {
-        console.log(`Successfully converted ${names[index]}.\n`);
+      } catch {
+        console.log(`Failed processing '${names[index]}'!\n`)
       }
-    } catch {
-      console.log(`Failed processing ${names[index]}!\n`);
-    }
-  }));
+    })
+  )
 
-  if (options.log) {
-    console.log(`Finished processing ${inputs.length} file(s)!`);
+  if (!options.quiet && inputs.length > 0) {
+    console.log(`Finished processing ${inputs.length} file(s)!`)
   }
 }
 
-main();
+main()
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { BinaryReader, BinaryWriter, Encoding } from 'csharp-binary-stream';
-import { buffer as toBuffer } from 'get-stream';
+import { BinaryReader, BinaryWriter, Encoding } from 'csharp-binary-stream'
+import { buffer as toBuffer } from 'get-stream'
 
-import JSZip from 'jszip';
-import lzma from 'lzma-purejs';
+import JSZip from 'jszip'
+import lzma from 'lzma-purejs'
 
 /*
   Extra interfaces have been defined in case I decide to expand this package.
@@ -11,19 +11,19 @@ import lzma from 'lzma-purejs';
 */
 
 export interface CFLEntry {
-  compression: number;
-  offset: number;
-  length: number;
-  size: number;
+  compression: number
+  offset: number
+  length: number
+  size: number
 
-  name: string;
-  hash: string | null;
+  name: string
+  hash: string | null
 
-  contents: ArrayBufferLike;
+  contents: ArrayBufferLike
 }
 
 export interface CFLOptions {
-  log?: boolean;
+  log?: boolean
 }
 
 /**
@@ -32,49 +32,50 @@ export interface CFLOptions {
  * @param options
  */
 export async function convert(buffer: ArrayBufferLike, options: CFLOptions = {}): Promise<Buffer> {
-  const reader = new BinaryReader(buffer);
+  const reader = new BinaryReader(buffer)
 
   // CFL Header
   // This information is exclusive to the CFL format
 
-  const hashed = reader.readChars(4, Encoding.Utf8) === 'DFL3';
+  const hashed = reader.readChars(4, Encoding.Utf8) === 'DFL3'
 
-  reader.position = reader.readUnsignedInt();
+  reader.position = reader.readUnsignedInt()
 
-  const format = reader.readInt();
+  const format = reader.readInt()
 
   if (options.log) {
-    console.log('Decoded CFL header.');
+    console.log('Decoded CFL header.')
   }
 
   // Decompress the LZMA section of the CFL file
-  const content = Uint8Array.from(reader.readBytes(reader.readInt()));
-  const data = new BinaryReader(format === 4 ? decompress(content) : content);
+  const content = Uint8Array.from(reader.readBytes(reader.readInt()))
+  const data = new BinaryReader(format === 4 ? decompress(content) : content)
 
   if (options.log) {
-    console.log('Decompressed CFL content.');
+    console.log('Decompressed CFL content.')
   }
 
   // Parse the CFL entries
-  const entries: CFLEntry[] = [];
+  const entries: CFLEntry[] = []
 
-  let position = 0;
+  let position = 0
   while (data.length > position) {
     // CFL entry header data
-    const size = data.readInt();
-    const offset = data.readInt();
-    const compression = data.readInt();
-    const name = data.readChars(data.readShort(), Encoding.Utf8);
+    const size = data.readInt()
+    const offset = data.readInt()
+    const compression = data.readInt()
+    const name = data.readChars(data.readShort(), Encoding.Utf8)
 
-    reader.position = offset;
+    reader.position = offset
 
     // Decompress the LZMA section of the entry
-    const entry = Uint8Array.from(reader.readBytes(reader.readUnsignedInt()));
-    const contents = compression === 4 ? decompress(entry) : entry;
+    const entry = Uint8Array.from(reader.readBytes(reader.readUnsignedInt()))
+    const contents = compression === 4 ? decompress(entry) : entry
 
     // Calculate hash and entry size
-    const hash: string | null = hashed ? data.readChars(data.readInt(), Encoding.Utf8) : null;
-    const length = 14 + name.length + (hash !== null && hash.match(/^\s*$/) === null ? 4 + hash.length : 0);
+    const hash: string | null = hashed ? data.readChars(data.readInt(), Encoding.Utf8) : null
+    const length =
+      14 + name.length + (hash !== null && hash.match(/^\s*$/) === null ? 4 + hash.length : 0)
 
     entries.push({
       compression,
@@ -84,22 +85,22 @@ export async function convert(buffer: ArrayBufferLike, options: CFLOptions = {})
       name,
       offset,
       size,
-    });
+    })
 
-    position += length;
+    position += length
   }
 
   if (options.log) {
-    console.log(`Parsed ${entries.length} CFL ${entries.length !== 1 ? 'entries' : 'entry'}.`);
+    console.log(`Parsed ${entries.length} CFL ${entries.length !== 1 ? 'entries' : 'entry'}.`)
   }
 
-  const zip = new JSZip();
+  const zip = new JSZip()
 
   entries.forEach((entry: CFLEntry) => {
-    zip.file(entry.name, entry.contents);
-  });
+    zip.file(entry.name, entry.contents)
+  })
 
-  return toBuffer(zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }));
+  return toBuffer(zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true }))
 }
 
 /**
@@ -107,10 +108,10 @@ export async function convert(buffer: ArrayBufferLike, options: CFLOptions = {})
  * @param data LZMA compressed data.
  */
 export function decompress(data: ArrayBufferLike): ArrayBufferLike {
-  const reader = new BinaryReader(data);
-  const writer = new BinaryWriter();
+  const reader = new BinaryReader(data)
+  const writer = new BinaryWriter()
   // Read the first 5 bytes, they are the properties
   // Set size to -1 to just read to EOS
-  lzma.decompress(reader.readBytes(5), reader, writer, -1);
-  return writer.toUint8Array();
+  lzma.decompress(reader.readBytes(5), reader, writer, -1)
+  return writer.toUint8Array()
 }
